def821618d50bec56d8a5de74589a5d257b24e45
Akir321 <anton-1945@bk.ru>
2023-10-05 15:51:58 +0300

    problem with switch solved
    added braces

--- spu.cpp
+++ spu.cpp
@@ -31,119 +31,20 @@ int runSPU(FILE *fin, FILE *fout)
     return EXIT_SUCCESS;
 }
 
-#if 1
-int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
-{   
-    if      (command == PUSH)
-    {
-        float value = 0;
-        if (fscanf(fin, "%f", &value) == 0) return INCORECT_PUSH;
-
-        stackPush(spuStack, (elem_t)(value * PrecisionConst));
-    }
-    else if (command == OUT)
-    {
-        elem_t value = 0;
-    
-        stackErrorField error = stackPop(spuStack, &value);
-        if (error.stack_underflow) return STACK_UNDERFLOW;
-
-        fprintf(fout, "%f\n", (float)value / (float)PrecisionConst);
-    }
-    else if (command == IN)
-    {
-        elem_t value1 = 0, value2 = 0;
-
-        stackErrorField error1 = stackPop(spuStack, &value1);
-        stackErrorField error2 = stackPop(spuStack, &value2);
-
-        if (error1.stack_underflow || error2.stack_underflow) return STACK_UNDERFLOW;
-
-        elem_t addit = value1 + value2;
-        stackPush(spuStack, addit);
-    }
-    else if (command == SUB)
-    {
-        elem_t subtrahend = 0, minuend = 0;
-
-        stackErrorField error1 = stackPop(spuStack, &subtrahend);
-        stackErrorField error2 = stackPop(spuStack, &minuend);
-
-        if (error1.stack_underflow || error2.stack_underflow) return STACK_UNDERFLOW;
-
-        elem_t subt = minuend - subtrahend;
-        stackPush(spuStack, subt);
-    }
-    else if (command == MUL)
-    {
-        elem_t value1 = 0, value2 = 0;
-
-        stackErrorField error1 = stackPop(spuStack, &value1);
-        stackErrorField error2 = stackPop(spuStack, &value2);
-
-        if (error1.stack_underflow || error2.stack_underflow) return STACK_UNDERFLOW;
-
-        elem_t mult = value1 * value2 / PrecisionConst;
-        stackPush(spuStack, mult);
-    }
-    else if (command == DIV)
-    {
-        elem_t dividend = 0, divisor = 0;
-
-        stackErrorField error1 = stackPop(spuStack, &divisor);
-        stackErrorField error2 = stackPop(spuStack, &dividend);
-
-        if (error1.stack_underflow || error2.stack_underflow) return STACK_UNDERFLOW;
-        if (divisor == 0)                                     return DIVISION_BY_ZERO;
-
-        elem_t divis = (dividend * PrecisionConst) / divisor;
-        stackPush(spuStack, divis);
-    }
-    else if (command == IN)
-    {
-        float value = 0;
-        static int valueNumber = 0;
-
-        printf("type in value (number %d) from console: ", ++valueNumber);
-        scanf("%f", &value);
-
-        stackPush(spuStack, (elem_t)(value * PrecisionConst));
-    }
-    else if (command == SQRT)
-    {
-        elem_t value = 0;
-
-        stackErrorField error = stackPop(spuStack, &value);
-        if (error.stack_underflow) return STACK_UNDERFLOW;
-
-        float sqRoot = sqrt((float)value / PrecisionConst);
-        stackPush(spuStack, (elem_t)(sqRoot * PrecisionConst));
-    }
-    else
-    {
-        fprintf(fout, "ERROR: unknown command: %d\n", command);
-        return UNKNOWN_COMMAND;
-    }
-
-    STACK_DUMP(spuStack);
-
-    return EXIT_SUCCESS;
-}
-#endif
-
-#if 0
 int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
 {
     switch(command)
     {
         case PUSH:
+        {
             float value = 0;
             if (fscanf(fin, "%f", &value) == 0) return INCORECT_PUSH;
 
             stackPush(spuStack, (elem_t)(value * PrecisionConst));
             break;
-
+        }
         case OUT:
+        {
             elem_t value = 0;
         
             stackErrorField error = stackPop(spuStack, &value);
@@ -151,8 +52,9 @@ int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
 
             fprintf(fout, "%f\n", (float)value / (float)PrecisionConst);
             break;
-
+        }
         case ADD:
+        {
             elem_t value1 = 0, value2 = 0;
 
             stackErrorField error1 = stackPop(spuStack, &value1);
@@ -163,8 +65,9 @@ int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
             elem_t addit = value1 + value2;
             stackPush(spuStack, addit);
             break;
-
+        }
         case SUB:
+        {
             elem_t subtrahend = 0, minuend = 0;
 
             stackErrorField error1 = stackPop(spuStack, &subtrahend);
@@ -175,8 +78,9 @@ int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
             elem_t subt = minuend - subtrahend;
             stackPush(spuStack, subt);
             break;
-
+        }
         case MUL:
+        {
             elem_t value1 = 0, value2 = 0;
 
             stackErrorField error1 = stackPop(spuStack, &value1);
@@ -187,8 +91,9 @@ int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
             elem_t mult = value1 * value2 / PrecisionConst;
             stackPush(spuStack, mult);
             break;
-            
+        }  
         case DIV:
+        {
             elem_t dividend = 0, divisor = 0;
 
             stackErrorField error1 = stackPop(spuStack, &divisor);
@@ -200,8 +105,9 @@ int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
             elem_t divis = (dividend * PrecisionConst) / divisor;
             stackPush(spuStack, divis);
             break;
-
+        }
         case IN:
+        {
             float value = 0;
             static int valueNumber = 0;
 
@@ -210,8 +116,9 @@ int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
 
             stackPush(spuStack, (elem_t)(value * PrecisionConst));
             break;
-
+        }
         case SQRT:
+        {
             elem_t value = 0;
 
             stackErrorField error = stackPop(spuStack, &value);
@@ -220,15 +127,16 @@ int processCommand(int command, stack *spuStack, FILE *fin, FILE *fout)
             float sqRoot = sqrt((float)value / PrecisionConst);
             stackPush(spuStack, (elem_t)(sqRoot * PrecisionConst));
             break;
-
+        }
         default:
+        {
             fprintf(fout, "ERROR: unknown command: %d\n", command);
             return UNKNOWN_COMMAND;
             break;
+        }
     }
 
     STACK_DUMP(spuStack);
 
     return EXIT_SUCCESS;
 }
-#endif
